7b5914f491314a78ed0f9e363f103e39cb93f65f
Cristo Sparks <50341397+crisparks@users.noreply.github.com>
2019-06-14 21:14:58 -0500

    Add files via upload

---
+++ FirstProgram.java
@@ -0,0 +1,7 @@
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("My name is Cristo Sparks");
+        System.out.println("I am 25 years old");
+        System.out.println("I was born in Dallas,Texas USA");
+    }
+}
